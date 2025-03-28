37c9bc6010345de532418b2d0fb1a23f929c2905
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-28 13:11:28 +0100

    feat: add response in principal route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.get('/', (req, res) => {
+    res.send('Home Server');
+});
+
 app.listen(port, () => {
     console.log(`Server attivo sulla port: ${port}`);
 });
